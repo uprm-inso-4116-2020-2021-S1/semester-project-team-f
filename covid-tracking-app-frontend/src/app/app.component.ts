24c354bd7aec799d7f3c0ef32a68fc4126cc3353
Edison <edison.zapata@upr.edu>
2020-10-31 20:58:57 -0400

    More front-end done on the doctos view

--- covid-tracking-app-frontend/src/app/app.component.ts
+++ covid-tracking-app-frontend/src/app/app.component.ts
@@ -8,13 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent{
   private static hasLoggedIn: boolean;
   private static addingPatient: boolean;
+  private static SeeingInformation: boolean;
+  private static ChangingAccountInformation: boolean;
   title = 'Coronavirus Tracking App';
 
   public isLoggedIn() { return AppComponent.hasLoggedIn; }
   public isAddingPatient() { return AppComponent.addingPatient; }
+  public isSeeingInformation()  { return AppComponent.SeeingInformation; }
+  public isChangingAccountInformation() { return AppComponent.ChangingAccountInformation; }
   public static changeToNavbar(){ AppComponent.hasLoggedIn = true; }
   public static changeToLogin() { AppComponent.hasLoggedIn = false; }
   public static changeToAddPatient() { AppComponent.addingPatient = true; }
+  public static changePatientInformation() { AppComponent.SeeingInformation = true; }
+  public static changeAccountInformation() { AppComponent.ChangingAccountInformation = true;}
+
+
   
 
 }
