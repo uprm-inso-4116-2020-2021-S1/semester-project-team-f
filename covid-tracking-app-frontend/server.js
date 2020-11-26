c3f79cc474deef7bc55719a1dc4e2931090f1035
felixdasta <felix.dasta@upr.edu>
2020-11-26 00:50:30 -0400

    Typos fixed and installed express

--- covid-tracking-app-frontend/server.js
+++ covid-tracking-app-frontend/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/covid-tracking-ap'));
+app.use(express.static(__dirname + '/dist/covid-tracking-app'));
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+
-    '/dist/covid-tracking-ap/index.html'));});
+    '/dist/covid-tracking-app/index.html'));});
 app.listen(process.env.PORT || 8080);
